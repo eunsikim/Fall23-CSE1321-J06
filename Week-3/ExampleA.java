c2df33c417a4161665de431ff1906fd44a924b3f
eunsikim <kimeunsik229@gmail.com>
2023-08-29 11:32:03 -0400

    Integer and float example

---
+++ Week-3/ExampleA.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ExampleA {
+    public static void main(String[] args) {
+        float x = 9f / 4f;
+
+        int y = 9 / 4;
+
+        System.out.println(x);
+
+        System.out.println(y);
+    }
+}
