f92a3b5eb6166d2c20c169001b340cd2f761bf17
eunsikim <kimeunsik229@gmail.com>
2023-09-19 11:45:55 -0400

    RNG Example

--- Week-6/ExampleB.java
+++ Week-6/ExampleB.java
@@ -20,7 +20,7 @@ public class ExampleB {
         while(engine && tank >= 0){
             if(tank > 0){
                 System.out.println("Iteration #" + (count++) + " & Tank : " + (tank * 100) + "%");
-                tank -= 0.05;           }
+            }
             else{
                 engine = false;
                 System.out.println("Tank is empty");
