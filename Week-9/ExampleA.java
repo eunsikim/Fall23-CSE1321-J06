fca86bedb2c665644b9989bd3f35631bf4e160e7
eunsikim <kimeunsik229@gmail.com>
2023-10-10 11:31:42 -0400

    nested for loop

---
+++ Week-9/ExampleA.java
@@ -0,0 +1,10 @@
+public class ExampleA {
+    public static void main(String[] args) {
+        for(int i = 0; i < 3; i++){
+            System.out.println("row = " + i);
+            for(int j = 0; j < 2; j++){
+                System.out.println("\tcolumn = " + j);
+            }
+        }
+    }
+}
