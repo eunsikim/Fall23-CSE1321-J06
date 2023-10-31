f71359f3e631cd017186d0c33a6a59b177b3c8ec
eunsikim <kimeunsik229@gmail.com>
2023-10-31 11:29:57 -0400

    String functions

---
+++ Week-12/ExampleA.java
@@ -0,0 +1,11 @@
+public class ExampleA {
+    public static void main(String[] args) {
+        String word = "Hello World";
+
+        System.out.println(word.substring(4));
+
+        for(int i = 0; i < word.length(); i++){
+            System.out.println(word.charAt(i));
+        }
+    }
+}
